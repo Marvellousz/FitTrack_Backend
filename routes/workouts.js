f85919e3678cc8e3168cde1c378c960109c90234
Marvellousz <pranavmurali024@gmail.com>
2024-11-28 12:57:50 +0000

    backend completed

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,23 +1,27 @@
-const express = require('express');
+const express = require('express')
 const {
-    createWorkout,
-    getWorkouts,
-    getWorkout,
-    deleteWorkout,
-    updateWorkout
-} = require('../controllers/workoutController');
+  getWorkouts, 
+  getWorkout, 
+  createWorkout, 
+  deleteWorkout, 
+  updateWorkout
+} = require('../controllers/workoutController')
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/', getWorkouts);
+// GET all workouts
+router.get('/', getWorkouts)
 
-router.get('/:id', getWorkout);
+// GET a single workout
+router.get('/:id', getWorkout)
 
+// POST a new workout
 router.post('/', createWorkout)
 
-router.delete('/:id', deleteWorkout);
+// DELETE a workout
+router.delete('/:id', deleteWorkout)
 
-router.patch('/:id', updateWorkout);
+// UPDATE a workout
+router.patch('/:id', updateWorkout)
 
-
-module.exports = router;
+module.exports = router
--- server.js
+++ server.js
@@ -2,12 +2,18 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const workoutRoutes = require('./routes/workouts.js')
 
 
 const app = express()
 
 app.use(express.json())
+app.use(cors(
+    {
+        origin: 'http://localhost:3000'
+    }
+))
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
