5ee1b17f855b159f9b22b3648473bffe401a94ed
Marvellousz <pranavmurali024@gmail.com>
2024-12-06 18:37:49 +0000

    auth setup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const workoutRoutes = require('./routes/workouts.js')
+const userRoutes = require('./routes/user.js')
 
 
 const app = express()
@@ -21,6 +22,7 @@ app.use((req, res, next) => {
 })
 
 app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
 mongoose.connect(process.env.MONG_URI)
     .then(() => {
